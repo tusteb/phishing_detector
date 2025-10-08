2b8703330c16978a8442e7f1f6add65ed004a89f
Margarita Stebeneva <tusteb@gmail.com>
2025-10-08 13:57:05 +0300

    Update app.py

--- ui/app.py
+++ ui/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import time
-from .predictor import predict
+from ui.predictor import predict
 
 st.set_page_config(page_title="Phishing Detector")
 st.markdown("# 📧 Phishing-detector")
@@ -20,3 +20,4 @@ with col2:
                         {message}
                     </div>
                     """, unsafe_allow_html=True)
+
